640264c0def79b0578711ee26d0bb1f6dc665f22
Ivan Alexandrov <mordrag.gothic@yandex.ru>
2023-09-20 14:43:38 +0300

    before github tests

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: 'stylelint-config-standard-scss',
+  "rules": {
+    "selector-class-pattern": null,
+    "custom-property-pattern": null,
+    "keyframes-name-pattern": null,
+  }
 };
